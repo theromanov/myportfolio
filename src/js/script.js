870de200c445637f19784f4106e0bd6e030d2b95
Roman Syplyvyi <romansyplyviy@gmail.com>
2025-12-29 18:48:10 +0100

    promo section

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 import "/src/sass/style.scss";
+
+const hamburger = document.querySelector(".hamburger"),
+  closeElem = document.querySelector(".menu__close"),
+  menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.add("menu_active");
+});
+
+closeElem.addEventListener("click", () => {
+  menu.classList.remove("menu_active");
+});
